ec7bc2b766451f873a373ca22abf8f851b56b085
gohjy <154421097+gohjy@users.noreply.github.com>
2025-11-15 17:33:47 +0000

    allow custom config in json

--- index.js
+++ index.js
@@ -4,8 +4,42 @@ import { setTimeout } from "node:timers/promises";
 
 const pageHTML = await fs.readFile("./page.html", { encoding: "utf-8" });
 
-const checkOnlineUrl = "https://upload.wikimedia.org/wikipedia/commons/f/f2/1px_trpt.png";
-const checkDmaUrl = "https://bloxd.io/textures/miscImages/logo.png";
+const config = await (async () => {
+    const userConfig = await fs.readFile("./config.json").then(JSON.parse).catch(() => ({}));
+    if (!userConfig || (typeof userConfig !== "object")) {
+        throw new Error("Invalid configuration in config.json");
+    }
+
+    const defaultConfig = {
+        checkOnlineUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f2/1px_trpt.png",
+        checkDmaUrl: "https://bloxd.io/textures/miscImages/logo.png"
+    };
+
+    const combinedConfig = Object.assign({}, defaultConfig, userConfig);
+
+    for (const key of Object.keys(defaultConfig)) {
+        if (
+            (typeof key === "string")
+            && key.match(/url/i) 
+            && URL.canParse(defaultConfig[key]) 
+            && !URL.canParse(combinedConfig[key])
+        ) {
+            console.warn(`WARNING: Configuration key "${key}" with value ${JSON.stringify(combinedConfig[key])} does not match URL syntax, defaulting to ${JSON.stringify(defaultConfig[key])}`);
+            combinedConfig[key] = defaultConfig[key];
+            continue;
+        }
+
+        if (
+            (typeof combinedConfig[key]) !== (typeof defaultConfig[key])
+        ) {
+            console.warn(`WARNING: Configuration key "${key}" with value ${JSON.stringify(combinedConfig[key])} does not match expected type "${typeof defaultConfig[key]}" (it is of type "${typeof combinedConfig[key]}"), defaulting to ${JSON.stringify(defaultConfig[key])}`);
+            combinedConfig[key] = defaultConfig[key];
+            continue;
+        }
+    }
+
+    return combinedConfig;
+})();
 
 async function resilientFetch(url) {
     try {
@@ -18,18 +52,22 @@ async function resilientFetch(url) {
     }
 }
 
+const PORT = 8080;
+
+console.log(`Starting check server on http://localhost:${PORT}/ with configuration: %o`, config);
+
 http.createServer(function (req, res) {
     if (req.url === "/") {
         res.writeHead(200, {"content-type": "text/html"});
         res.write(pageHTML);
         res.end();
     } else if (req.url === "/check/online") {
-        resilientFetch(checkOnlineUrl)
+        resilientFetch(config.checkOnlineUrl)
         .then(() => {res.writeHead(200)})
         .catch(() => {res.writeHead(418)})
         .finally(() => {res.end()})
     } else if (req.url === "/check/dma") {
-        resilientFetch(checkDmaUrl)
+        resilientFetch(config.checkDmaUrl)
         .then(() => {res.writeHead(200)})
         .catch(() => {res.writeHead(418)})
         .finally(() => {res.end()})
@@ -37,4 +75,4 @@ http.createServer(function (req, res) {
         res.writeHead(404);
         res.end();
     }
-}).listen(8080);
+}).listen(PORT);
